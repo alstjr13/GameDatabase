caff5f787c962f26a6188cc26486f506e557b9a2
UBC Student <zzhang61@students.cs.ubc.ca>
2023-11-30 11:10:46 -0800

    Connection Pooling (#2)
    * connection pooling
    
    * adjusted max pool number
    
    ---------
    
    Co-authored-by: Chloe Zhang <zzhang61@student.ubc.ca>

--- appService.js
+++ appService.js
@@ -7,8 +7,40 @@ const envVariables = loadEnvFile('./.env');
 const dbConfig = {
     user: envVariables.ORACLE_USER,
     password: envVariables.ORACLE_PASS,
-    connectString: `${envVariables.ORACLE_HOST}:${envVariables.ORACLE_PORT}/${envVariables.ORACLE_DBNAME}`
+    connectString: `${envVariables.ORACLE_HOST}:${envVariables.ORACLE_PORT}/${envVariables.ORACLE_DBNAME}`,
+    poolMin: 1,
+    poolMax: 3,
+    poolIncrement: 1,
+    poolTimeout: 60
 };
+
+// initialize connection pool
+async function initializeConnectionPool() {
+    try {
+        await oracledb.createPool(dbConfig);
+        console.log('Connection pool started');
+    } catch (err) {
+        console.error('Initialization error: ' + err.message);
+    }
+}
+
+async function closePoolAndExit() {
+    console.log('\nTerminating');
+    try {
+        await oracledb.getPool().close(10); // 10 seconds grace period for connections to finish
+        console.log('Pool closed');
+        process.exit(0);
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+initializeConnectionPool();
+
+process
+    .once('SIGTERM', closePoolAndExit)
+    .once('SIGINT', closePoolAndExit);
 
 
 // ----------------------------------------------------------
@@ -16,7 +48,7 @@ const dbConfig = {
 async function withOracleDB(action) {
     let connection;
     try {
-        connection = await oracledb.getConnection(dbConfig);
+        connection = await oracledb.getConnection(); // Gets a connection from the default pool 
         return await action(connection);
     } catch (err) {
         console.error(err);
